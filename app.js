9c39d8793f7bc469d7cb416dc2d558eabed68490
VaderRob <69919131+VaderRob@users.noreply.github.com>
2020-12-06 17:26:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+let heldValue = null;
+let heldOperation = null;
+let nextValue = null;
+
+$('.digits button').click(function() {
+    if (nextValue === null) {
+        nextValue === "0";
+    }   
+    nextValue += $(this).text();
+    $(this).text();
+}
+
+
+function showValue(location , value) {
+    if(location === null) {
+        $(location) === "";
+    } else {
+        $(location) === value;
+    }
+}
+
+showValue();
+
+function updateDisplay() {
+
+}
+
+updateDisplay();
+
+function add(x, y) {
+    function setHeldOperation(operation) {
+
+    }
+}
+
+add();
+
+function subtract(x, y) {
+    function setHeldOperation(operation) {
+
+    }
+}
+
+subtract();
+
+function multiply(x, y) {
+    function setHeldOperation(operation) {
+
+    }
+}
+
+multiply();
+
+function divide(x, y) {
+    function setHeldOperation(operation) {
+
+    }
+}
+
+divide();
+
+
+
+
+$('.add').click(function () {
+
+})
+
+$('.subtract').click(function () {
+    
+})
+
+$('.multiply').click(function () {
+    
+})
+
+$('.divide').click(function () {
+    
+})
+
+$('.equals').click(function () {
+    
+})
+
+$('.clear-all').click(function () {
+    showValue === null;
+    heldValue === null;
+    nextValue === null;
+
+})
+
+$('.clear-entry').click(function () {
+    nextValue === null;
+})
+
